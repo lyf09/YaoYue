67a1f7b3e1215f979f3d8e2bb0eb82a06efd6d67
lyf09 <2331692234@qq.com>
2017-10-26 21:29:36 +0800

    firstchage

--- app/src/main/java/com/example/administrator/yaoyue/network/UserService.java
+++ app/src/main/java/com/example/administrator/yaoyue/network/UserService.java
@@ -3,6 +3,7 @@ package com.example.administrator.yaoyue.network;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 /**
  * Created by Administrator on 2017/10/26.
@@ -10,5 +11,8 @@ import retrofit2.http.Path;
 
 public interface UserService {
     @GET("LoginAction")
-    Call<List<Repo>> login(@Path("userid") String userid, @Path("userpassword") String userpassword);
+    Call<List<Repo>> login(@Query("userid") String userid, @Query("userpassword") String userpassword);
+
+    @GET("RegisterAction")
+    Call<List<Repo>> register(@Query("userid") String userid, @Query("userpassword") String userpassword, @Query("email") String email);
 }
